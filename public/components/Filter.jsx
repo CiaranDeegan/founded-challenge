2d5ae2a1dce3782350b334a005722f3ebc4881dc
Ciaran Deegan <ciarandee@gmail.com>
2018-04-03 19:11:56 -0400

    implemented filter toggle

--- public/components/Filter.jsx
+++ public/components/Filter.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showCats: true,
+            showSharks: true
+        };
+
+        this.toggleState = this.toggleState.bind(this);
+    }
+
+    toggleState(field) {
+        var state = {};
+        state[field] = !this.state[field];
+        this.setState(state);
+    }
+
     render() {
         return (
             <div>
-                <button>Cats</button>
-                <button>Sharks</button>
+                <button onClick={() => this.toggleState('showCats')}>{this.state.showCats ? 'HIDE CATS' : 'SHOW CATS'}</button>
+                <button onClick={() => this.toggleState('showSharks')}>{this.state.showSharks ? 'HIDE SHARKS' : 'SHOW SHARKS'}</button>
             </div>
         );
     }
+
 }
 
 export default Gallery;
