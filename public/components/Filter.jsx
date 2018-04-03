c65f3b6bdf5884d6b1832ff29346326912f35793
Ciaran Deegan <ciarandee@gmail.com>
2018-04-03 19:30:08 -0400

    moved filter toggles to parent component

--- public/components/Filter.jsx
+++ public/components/Filter.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 
-class Gallery extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showCats: true,
-            showSharks: true
-        };
-
-        this.toggleState = this.toggleState.bind(this);
-    }
-
-    toggleState(field) {
-        var state = {};
-        state[field] = !this.state[field];
-        this.setState(state);
-    }
-
+class Filter extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={() => this.toggleState('showCats')}>{this.state.showCats ? 'HIDE CATS' : 'SHOW CATS'}</button>
-                <button onClick={() => this.toggleState('showSharks')}>{this.state.showSharks ? 'HIDE SHARKS' : 'SHOW SHARKS'}</button>
+                <button onClick={() => this.props.toggleFilterState('showCats')}>Cats</button>
+                <button onClick={() => this.props.toggleFilterState('showSharks')}>Sharks</button>
             </div>
         );
     }
 
 }
 
-export default Gallery;
+export default Filter;
--- public/components/Gallery.jsx
+++ public/components/Gallery.jsx
@@ -3,10 +3,27 @@ import Filter from './Filter.jsx';
 import Carousel from './Carousel.jsx';
 
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showCats: true,
+            showSharks: true
+        };
+
+        this.toggleFilterState = this.toggleFilterState.bind(this);
+    }
+
+    toggleFilterState(field) {
+        var state = {};
+        state[field] = !this.state[field];
+        this.setState(state);
+    }
+
     render() {
         return (
             <div>
-                <Filter/>
+                <Filter toggleFilterState={this.toggleFilterState}/>
                 <Carousel/>
             </div>
         );
