41df27fced6ace21ee26f464cdad5fbed3b97873
Ciaran Deegan <ciarandee@gmail.com>
2018-04-03 18:28:51 -0400

    component get images from API

--- public/components/Carousel.jsx
+++ public/components/Carousel.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import HTTPClient from '../js/http.js';
 
 class Carousel extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            imageURLs: ['https://founded.media/hiring/photos/cats/14157413946_fea785b4d6_k.jpg',
-            'https://founded.media/hiring/photos/cats/16175483119_bd7374d8a8_h.jpg',
-            'https://founded.media/hiring/photos/cats/13901304865_a444cf4d34_k.jpg',
-            'https://founded.media/hiring/photos/cats/8311701653_49ed80202c_k.jpg',
-            'https://founded.media/hiring/photos/cats/13336301695_3c06dd41cc_k.jpg',
-            'https://founded.media/hiring/photos/cats/38679744435_66279af67c_k.jpg',
-            'https://founded.media/hiring/photos/cats/6393395037_9cda69da1a_b.jpg',
-            'https://founded.media/hiring/photos/cats/6977309082_44102ddf51_b.jpg',
-            'https://founded.media/hiring/photos/cats/11477923503_bbdf86387d_b.jpg',
-            'https://founded.media/hiring/photos/cats/4481336172_7f464f180d_b.jpg']
+            imageURLs: []
         };
     }
     render() {
@@ -23,6 +15,16 @@ class Carousel extends React.Component {
                 <img src={this.state.imageURLs[Math.floor(Math.random() * (this.state.imageURLs.length))]}/>
             </div>
         );
+    }
+    componentDidMount() {
+        //load images into state
+        let component = this;
+        HTTPClient.get('/api/cats', function(res) {
+            console.log(JSON.parse(res));
+            component.setState({
+                imageURLs: JSON.parse(res).urls
+            });
+        });
     }
 }
 
