a4122e54313eee560a974b6e51378ba844bb6eb0
Ciaran Deegan <ciarandee@gmail.com>
2018-04-03 23:22:07 -0400

    css module configuration

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
                         presets: ['react']
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
             }
         ]
     },
