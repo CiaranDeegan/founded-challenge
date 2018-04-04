f97a4dd8babb775607bfdff294af7b3c587a7e18
Ciaran Deegan <ciarandee@gmail.com>
2018-04-04 00:46:37 -0400

    improved carousel arrow colouring

--- public/components/Carousel.jsx
+++ public/components/Carousel.jsx
@@ -5,11 +5,11 @@ class Carousel extends React.Component {
     render() {
         return (
             <div className={styles.carousel}>
-                <button className="button is-info" onClick={() => this.props.adjustIndex(-1)}>&lt;</button>
+                <button className="button is-warning" onClick={() => this.props.adjustIndex(-1)}>&lt;</button>
                 <figure className="image">
                     <img src={this.props.imageURLs[this.props.index]}/>
                 </figure>
-                <button className="button is-info" onClick={() => this.props.adjustIndex(1)}>&gt;</button>
+                <button className="button is-warning" onClick={() => this.props.adjustIndex(1)}>&gt;</button>
             </div>
         );
     }
