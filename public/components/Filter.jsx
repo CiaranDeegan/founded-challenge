683b7f55b155072520378f60e84115eaee528121
Ciaran Deegan <ciarandee@gmail.com>
2018-04-03 23:22:32 -0400

    added filter component css

--- public/components/Filter.jsx
+++ public/components/Filter.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Filter.css'
 
 class Filter extends React.Component {
     render() {
         return (
-            <div className="has-text-centered">
+            <div className={styles.filter + ' has-text-centered'}>
                 <button className={this.props.showCats ? 'button is-primary': 'button'}  onClick={() => this.props.toggleFilterState('showCats')}>Cats</button>
                 <button className={this.props.showSharks ? 'button is-primary': 'button'} onClick={() => this.props.toggleFilterState('showSharks')}>Sharks</button>
             </div>
